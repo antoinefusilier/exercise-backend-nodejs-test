6573f75757657343ff6aabae9636d1f645e57997
antoinefusilier <antoinefusilier@gmail.com>
2022-12-16 16:48:31 +0000

    vend commit

--- components/componentManager/componentManager.js
+++ components/componentManager/componentManager.js
@@ -8,6 +8,10 @@ class ComponentManager {
         let ComponentManagerController = require('./componentManagerController');
         this.ComponentManagerController = new ComponentManagerController(app);
         if (action == 'add') this.ComponentManagerController.addComponent(component);
+        if (action == 'remove') this.ComponentManagerController.removeComponent(component);
+        // if (action == 'update') this.ComponentManagerController.updateComponent(component);
+        // if (action == 'get') this.ComponentManagerController.getComponent(component);
+        if (action == 'init') this.ComponentManagerController
     }
 }
 module.exports = ComponentManager;
